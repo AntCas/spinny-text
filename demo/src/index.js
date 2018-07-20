de4c139430c96eaf2b12dec7c36e698b72f29145
Anthony Castrio <anthonycastrio@gmail.com>
2018-07-20 16:03:25 +0200

    update demo with better styles

--- demo/src/index.js
+++ demo/src/index.js
@@ -4,8 +4,22 @@ import { render } from 'react-dom';
 import SpinnyText from 'spinny-text';
 
 render(
-  <div style={{ textAlign: 'center' }}>
-    <h2 style={{ fontSize: 16, color: 'black' }}>
+  <div style={{
+    background: 'black',
+    height: '100vh',
+    width: '100vw',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }}>
+    <h2 style={{
+      fontSize: 48,
+      color: 'white'
+    }}>
       <SpinnyText text="React Spinny Text Demo" />
     </h2>
   </div>, 
